fc0df42cff92887bc013ba006b9cc277605eb74e
sankalpramteke <sankalpramteke74@gmail.com>
2024-11-23 15:44:41 +0530

    #Day-01

---
+++ Basics_01/conversionandoperations.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+console.log(typeof(score));
+
+let valueInNumeber = Number(score)  //its convert string into number
+
+console.log(typeof(valueInNumeber));
+
+//"33" : 33
+//"33abc" : NaN
+//"true : 1 , false : 0"
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 : true , 0 : false
+// "" : false
+// "Sankalp" : true
+
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+ 
