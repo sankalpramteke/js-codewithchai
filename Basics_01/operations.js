09f6ec72ddf03aea6db2bb39d0abd57be6155dc8
sankalpramteke <sankalpramteke74@gmail.com>
2024-11-24 08:40:34 +0530

    #Day-02

---
+++ Basics_01/operations.js
@@ -0,0 +1,51 @@
+let value = 3
+let negVal = -value
+//console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " sankalp"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+
+//learn prefix and postfix
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
