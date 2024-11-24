5505187cdb4b89b689022da55f13905f8e0f4383
sankalpramteke <sankalpramteke74@gmail.com>
2024-11-24 09:29:19 +0530

    #Day-02

---
+++ Basics_01/stackheap.js
@@ -0,0 +1,21 @@
+// Stck (Primitive) , Heap (Non-Primitive)
+
+let myYtName = "Sankalp"
+let anothername = myYtName
+anothername = "sankalpramteke"
+
+console.log(myYtName);
+console.log(anothername);
+
+let userOne = {
+    email : "gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sankalp.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
